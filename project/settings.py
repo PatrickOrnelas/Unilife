a1d5ac73691158f9764e9cbd22bfca80775b970b
davi <dawidrizzofernandes@hotmail.com>
2025-09-02 18:39:40 -0300

    .

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contas',
-    "livereload",
 ]
 
 MIDDLEWARE = [
@@ -38,7 +37,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "livereload.middleware.LiveReloadScript",
 ]
 
 ROOT_URLCONF = 'project.urls'
